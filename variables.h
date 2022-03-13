9740692fb9a9ea167969602a1b9371e8634ed5e6
Andrew Dansby <andydansby@gmail.com>
2022-03-13 12:08:47 -0400

    Add files via upload

---
+++ variables.h
@@ -0,0 +1,21 @@
+signed int x, y;
+float t;
+
+unsigned char yHeight = 191;//height of the display
+unsigned char xWidth = 255;//width of the display
+
+
+long microSec;
+long seconds;
+long minutes;
+long timeStart;
+long timeEnd;
+long timeDiff;
+
+unsigned char xx = 0;
+unsigned char yy = 0;
+
+unsigned int temp;
+
+
+static unsigned short jz,jsr=5;//used in randomizer
