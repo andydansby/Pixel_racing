08d137e0dcc4ad8574565a3025e10343f1a50d28
Andrew Dansby <andydansby@gmail.com>
2025-04-26 00:54:25 -0400

    Add files via upload
    further optimization of hella_plot 166 T-states per pixel

--- asm_files/hella_plot.asm
+++ asm_files/hella_plot.asm
@@ -1,6 +1,56 @@
-
 PUBLIC _hellaPlot
-_hellaPlot:          ; plot d = x-axis, e = y-axis
+_hellaPlot:	              ;  plot d = x-axis, e = y-axis
+	                      ;  166 T states per pixel
+	ld DE,(_gfx_xy)       ;  load xy pair
+	xor A                 ;  reset a to 0 and flags to default
+
+	ld A,E                ;  load X plot point
+
+	rra                   ;  rotate Right ----------- divide in half
+	                      ;  this could also be an RRCA
+	scf                   ;  set carry flag --------- turn on Carry flag
+	rra                   ;  rotate right
+	or A
+	rra                   ;  rotate right
+	                      ;  this could also be an RRCA
+
+	ld L,A                ;  temp store in L
+	xor E
+	and %11111000         ;  mask out unwanted bits
+	xor E
+
+	ld H,A                ;  store High address
+
+
+	ld A,D                ;  load Y plot point
+	xor L
+	and %00000111         ;  mask out unwanted bits
+	xor D
+	rrca                  ;  must be rrca
+	rrca                  ;  must be rrca
+	rrca                  ;
+
+	ld L,A
+	                      ;  now we have the full address
+	                      ;  now use LUT to find which bit to set
+	ld A,D
+	and %00000111 ; mask out unwanted bits
+
+	                      ;  use a LUT to quickly find the bit position for the X position
+	ld DE,X_PositionBits
+	add A,E
+	ld E,A
+	ld A,(DE)
+
+	                      ;  output to screen
+	or (HL)
+	ld (HL),A
+ret
+
+
+
+PUBLIC _hellaPlot_old
+_hellaPlot_old:          ; plot d = x-axis, e = y-axis
     ld de, (_gfx_xy)
     ld a,7
     and d
@@ -41,8 +91,6 @@ _hellaPlot:          ; plot d = x-axis, e = y-axis
     or (hl)
     ld (hl),a
 ret
-
-
 
 
 ;Fastcall only supports one parameter in DEHL
